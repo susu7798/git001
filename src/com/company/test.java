7e3d32b70326d9a62865cafde754121bef077008
susu7798 <3626342290@qq.com>
2020-12-25 15:07:36 +0800

    今天星期五

---
+++ src/com/company/test.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * @author 罗阳
+ * @version 1.0
+ * @description 一度教育
+ * @date 2020/12/2515:05
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("so~");
+    }
+
+}
